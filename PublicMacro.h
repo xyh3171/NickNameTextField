0172457653dc27451af7ee7e6fc4f73eacb11cf5
xyh3171 <xyh3171@gmail.com>
2018-09-03 23:48:09 +0800

    macro

---
+++ PublicMacro.h
@@ -0,0 +1,20 @@
+//
+//  PublicMacro.h
+//  NickNameTextField
+//
+//  Created by xuyonghua on 2018/9/3.
+//  Copyright © 2018 FN. All rights reserved.
+//
+
+#ifndef PublicMacro_h
+#define PublicMacro_h
+
+#pragma mark - Frame
+#define SCR_WIDTH                  [UIScreen mainScreen].bounds.size.width
+#define SCR_HEIGHT                 [UIScreen mainScreen].bounds.size.height
+#define RECT(x, y, w, h)           CGRectMake(x, y, w, h)
+
+#pragma mark - Font
+#define FONT(SIZE)                 [UIFont systemFontOfSize:SIZE]
+
+#endif /* PublicMacro_h */
